1205ba029c8ab08a25253d30958fd22a4c8da28c
lajkogege <lajkogege@gmail.com>
2024-12-20 10:57:28 +0100

    HÁROMtÁBLA CM LÉTREHOZÁSA KULCSOKKAL

---
+++ database/migrations/2024_12_20_092636_create_basket_extras_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('basket_extras', function (Blueprint $table) {
+            $table->unsignedBigInteger('basket_id'); 
+            $table->unsignedBigInteger('item_id');  
+            $table->integer('ingredient');          
+            $table->boolean('modification_type')->default(0);
+            $table->integer('quantity');
+        
+            // Összetett kulcs
+            $table->primary(['basket_id', 'item_id', 'ingredient']);
+
+            $table->timestamps();
+        
+            // Idegen kulcsok beállítása
+            $table->foreign(['basket_id', 'item_id'])
+                  ->references(['basket_id', 'item_id']) // Pontos mezőnevek
+                  ->on('basket_items')
+                  ->onDelete('cascade');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('basket_extras');
+    }
+};
