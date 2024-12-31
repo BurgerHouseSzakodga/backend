f836cf514bf743b8d4e294c70202b1550cefa47e
KAkos27 <akoskosztolanyi@gmail.com>
2024-12-31 16:32:29 +0100

    navannakkepek

--- database/migrations/2024_12_20_094517_create_menu_items_table.php
+++ database/migrations/2024_12_20_094517_create_menu_items_table.php
@@ -22,15 +22,14 @@ return new class extends Migration
             $table->integer('price');
             $table->timestamps();
         });
-        MenuItem::create(['name' => 'sajtburger', 'description' => 'Marhahúspogácsa zsemlében, ömlesztett cheddar sajtszelettel, savanyú uborkával, hagymával, ketchuppal és mustárral.', 'category_id' => 1, 'image_path' => 'aa', 'price' => 1490]);
-        MenuItem::create(['name' => 'baconsajtburger', 'description' => 'Marhahúspogácsa zsemlében, bacon szeletekkel, cheddar sajttal, salátával és BBQ szósszal.', 'category_id' => 1, 'image_path' => 'bb', 'price' => 1690]);
-        MenuItem::create(['name' => 'csirkeburger', 'description' => 'Ropogós csirkemell zsemlében, salátával, paradicsommal és majonézzel.', 'category_id' => 1, 'image_path' => 'cc', 'price' => 1390]);
-        MenuItem::create(['name' => 'vega burger', 'description' => 'Zöldségpogácsa zsemlében, salátával, paradicsommal, uborkával és vegán majonézzel.', 'category_id' => 1, 'image_path' => 'dd', 'price' => 1290]);
-        MenuItem::create(['name' => 'dupla sajtburger', 'description' => 'Két marhahúspogácsa, dupla cheddar sajttal, savanyú uborkával és hagymával.', 'category_id' => 1, 'image_path' => 'ee', 'price' => 1990]);
-        MenuItem::create(['name' => 'chilis burger', 'description' => 'Marhahúspogácsa, jalapeno paprikával, cheddar sajttal, salátával és csípős szósszal.', 'category_id' => 1, 'image_path' => 'ff', 'price' => 1590]);
-        MenuItem::create(['name' => 'halas burger', 'description' => 'Ropogós halfilé zsemlében, salátával, tartármártással és citrommal.', 'category_id' => 1, 'image_path' => 'gg', 'price' => 1490]);
-        MenuItem::create(['name' => 'grill szendvics', 'description' => 'Grillezett csirkemell, pirított zsemle, salátával, paradicsommal és majonézzel.', 'category_id' => 1, 'image_path' => 'hh', 'price' => 1590]);
-        MenuItem::create(['name' => 'gyerek menü', 'description' => 'Mini sajtburger, sült krumpli és gyümölcslé.', 'category_id' => 1, 'image_path' => 'jj', 'price' => 1190]);
+        MenuItem::create(['name' => 'sajtburger', 'description' => 'Marhahúspogácsa zsemlében, ömlesztett cheddar sajtszelettel, savanyú uborkával, hagymával, ketchuppal és mustárral.', 'category_id' => 1, 'image_path' => 'http://localhost:8000/images/cheeseburger.jpg', 'price' => 1490]);
+        MenuItem::create(['name' => 'baconsajtburger', 'description' => 'Marhahúspogácsa zsemlében, bacon szeletekkel, cheddar sajttal, salátával és BBQ szósszal.', 'category_id' => 1, 'image_path' => 'http://localhost:8000/images/baconburger.jpg', 'price' => 1690]);
+        MenuItem::create(['name' => 'csirkeburger', 'description' => 'Ropogós csirkemell zsemlében, salátával, paradicsommal és majonézzel.', 'category_id' => 1, 'image_path' => 'http://localhost:8000/images/chickenburger.jpg', 'price' => 1390]);
+        MenuItem::create(['name' => 'vega burger', 'description' => 'Zöldségpogácsa zsemlében, salátával, paradicsommal, uborkával és vegán majonézzel.', 'category_id' => 1, 'image_path' => 'http://localhost:8000/images/vegan-burger.jpg', 'price' => 1290]);
+        MenuItem::create(['name' => 'dupla sajtburger', 'description' => 'Két marhahúspogácsa, dupla cheddar sajttal, savanyú uborkával és hagymával.', 'category_id' => 1, 'image_path' => 'http://localhost:8000/images/double-cheeseburger.jpg', 'price' => 1990]);
+        MenuItem::create(['name' => 'chilis burger', 'description' => 'Marhahúspogácsa, jalapeno paprikával, cheddar sajttal, salátával és csípős szósszal.', 'category_id' => 1, 'image_path' => 'http://localhost:8000/images/chiliburger.jpg', 'price' => 1590]);
+        MenuItem::create(['name' => 'halas burger', 'description' => 'Ropogós halfilé zsemlében, salátával, tartármártással és citrommal.', 'category_id' => 1, 'image_path' => 'http://localhost:8000/images/fishburger.jpg', 'price' => 1490]);
+        MenuItem::create(['name' => 'grill szendvics', 'description' => 'Grillezett csirkemell, pirított zsemle, salátával, paradicsommal és majonézzel.', 'category_id' => 1, 'image_path' => 'http://localhost:8000/images/grill-sandwich.jpg', 'price' => 1590]);
     }
 
     /**
